e31d7c29ce6ce4fa40dd881e6e4d75709968111d
abdullahaaf1 <abdullahaaf1@gmail.com>
2018-12-26 11:29:01 +0700

    codes to solve problem python swap-case

---
+++ python-swap-case.py
@@ -0,0 +1,37 @@
+#PROBLEM
+'''
+You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+For Example:
+
+Www.HackerRank.com → wWW.hACKERrANK.COM
+Pythonist 2 → pYTHONIST 2
+Input Format
+
+A single line containing a string .
+
+Constraints
+
+
+Output Format
+
+Print the modified string .
+
+Sample Input 0
+
+HackerRank.com presents "Pythonist 2".
+Sample Output 0
+
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+
+Note : The swapcase() method returns the string where all uppercase characters are converted to lowercase, and lowercase characters are converted to uppercase.
+
+'''
+#SOLUTION
+def swap_case(s):
+	return s.swapcase()
+
+if __name__ == '__main__':
+	s = input()
+	result = swap_case(s)
+	print(result)
